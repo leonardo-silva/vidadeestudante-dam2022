dab90be9de8ba4d811702bc5acf7ed1301cc3115
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-03-23 13:59:46 -0300

    Configuracao para TypeScript com styled-components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
